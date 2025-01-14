1a066ad4ca888afd987a5ed192cb567b5d0e8998
mese <mesegirones97@gmail.com>
2025-01-14 19:20:54 +0100

    feat: add inline documentation

--- question/service.question.go
+++ question/service.question.go
@@ -32,9 +32,11 @@ func NewService(loggerProxy LoggerProxy, openaiProxy OpenaiProxy) *Service {
 // Validating input and generating analysis
 func (s *Service) ProcessQuestion(ctx context.Context, input *domain.QuestionInput) (domain.QuestionResponse, error) {
 
+	//Create response that will be the result of this function.
 	response := domain.QuestionResponse{}
 	response.Channel = make(chan domain.QuestionStatus)
 
+	//Validate input, and sending an error.
 	if input == nil {
 		go s.InputError(response.Channel)
 	}
@@ -51,6 +53,7 @@ func (s *Service) InputError(channel chan domain.QuestionStatus) {
 
 // Main analysis query
 func (s *Service) AnalyzeQuestion(ctx context.Context, channel chan domain.QuestionStatus, input *domain.QuestionInput) {
+
 	language, summary := s.FirstStep(ctx, channel, input.Question)
 	response := s.SecondStep(ctx, channel, language, summary)
 
@@ -61,12 +64,15 @@ func (s *Service) AnalyzeQuestion(ctx context.Context, channel chan domain.Quest
 
 // First step of the analysis. Calls de first two LLM and returns the results in string format.
 func (s *Service) FirstStep(ctx context.Context, channel chan domain.QuestionStatus, userInput string) (string, string) {
+
+	//Results of the two LLM calls will be collected here.
 	results := make(chan domain.LLMOutput, 2)
 
 	var wgFirstStep sync.WaitGroup
 
 	wgFirstStep.Add(2)
 
+	//Setup pormpts and data type for each LLM call. This could be moved to a config file.
 	prompts := []domain.LLMInput{
 		{
 			Prompt:   "In one word, wich is the main point of the following text: %s",
@@ -78,6 +84,7 @@ func (s *Service) FirstStep(ctx context.Context, channel chan domain.QuestionSta
 		},
 	}
 
+	//LLM calls
 	for i, input := range prompts {
 		status := "Invoking LLM " + strconv.Itoa(i+1)
 		channel <- domain.QuestionStatus{Status: status, Message: ""}
@@ -87,8 +94,10 @@ func (s *Service) FirstStep(ctx context.Context, channel chan domain.QuestionSta
 
 	wgFirstStep.Wait()
 
+	//Close results channel
 	close(results)
 
+	// Collect results depending of the data type.
 	var language, summary string
 	for result := range results {
 		switch result.DataType {
@@ -103,6 +112,7 @@ func (s *Service) FirstStep(ctx context.Context, channel chan domain.QuestionSta
 }
 
 // Calls the third LLM and returns result in string format.
+// COMMENT: this function and the previous onse seem very similar, thus everythig could be put together in a more generic function. This could expect an array configurations and return an array of responses, depending of the data types of the configurations.
 func (s *Service) SecondStep(ctx context.Context, channel chan domain.QuestionStatus, language string, summary string) string {
 	results := make(chan domain.LLMOutput, 1)
 
@@ -126,7 +136,7 @@ func (s *Service) SecondStep(ctx context.Context, channel chan domain.QuestionSt
 	return response
 }
 
-// Perform LLM query
+// Perform LLM query.
 func (s *Service) LLM(ctx context.Context, wg *sync.WaitGroup, results chan domain.LLMOutput, promt string, dataType domain.DataTypeEnum) {
 	defer wg.Done()
 
